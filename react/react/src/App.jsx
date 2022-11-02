66154ea977a7613d575590d09c078a321262f383
Luiz Eduardo <luizeduardorodrigues807@gmail.com>
2022-11-02 16:19:04 -0300

    componente Header Add

---
+++ react/react/src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./components/Header/Header";
+
+import "./App.css";
+
+const App = () => {
+    return (
+        <div className="App">
+            <Header />
+        </div>
+    );
+};
+
+export default App;
