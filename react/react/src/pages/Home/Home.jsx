d372365797c8ef0acbdf2b6c0494ef8065b5049c
Luiz Eduardo <luizeduardorodrigues807@gmail.com>
2022-11-14 12:00:06 -0300

    Home router add

---
+++ react/react/src/pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+import style from "./Home.module.css";
+
+const Home = () => {
+    return (
+        <div className={style.container_home}>
+            <h1>Home</h1>
+            <p>Página Inicial</p>
+        </div>
+    );
+};
+
+export default Home;
