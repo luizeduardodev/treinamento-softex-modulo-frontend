d9ca63b6295cc0c68443711e84000a4adf75443a
Luiz Eduardo <luizeduardorodrigues807@gmail.com>
2022-11-14 12:00:52 -0300

    Contact router add

---
+++ react/react/src/pages/Contact/Contact.jsx
@@ -0,0 +1,12 @@
+import style from "./Contact.module.css";
+
+const Contact = () => {
+    return (
+        <div className={style.container_contact}>
+            <h1>Contact</h1>
+            <p>Página Contact</p>
+        </div>
+    );
+};
+
+export default Contact;
