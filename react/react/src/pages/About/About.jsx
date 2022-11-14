67d1a367a403f43936309bbc691562c6132a4589
Luiz Eduardo <luizeduardorodrigues807@gmail.com>
2022-11-14 12:01:42 -0300

    About router add

---
+++ react/react/src/pages/About/About.jsx
@@ -0,0 +1,13 @@
+import style from "./About.module.css";
+
+const About = () => {
+    return (
+        <div className={style.container_about}>
+            <h1>About</h1>
+            <p>Página About</p>
+        </div>
+    );
+};
+
+export default About;
+
